2f073883a0e23f8cc2898392d42096a234ea7957
yav-d <yavor.d0@gmail.com>
2021-10-22 16:08:26 +0300

    vae training wip

--- model/gen_model/vae.py
+++ model/gen_model/vae.py
@@ -1,12 +1,8 @@
-from keras.layers import Input, Dense, Lambda
-from keras.models import Model
-from keras.objectives import binary_crossentropy
-from keras.callbacks import LearningRateScheduler
-
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import keras.backend as K
-import tensorflow as tf
+
+# tf.debugging.set_log_device_placement(True)
 
 class VAE():
     def __init__(
@@ -19,32 +15,35 @@ class VAE():
         n_cont_cov: int = 0,
         dropout_rate: float = 1e-3
     ):
-        self.n_input = n_input,
-        self.n_labels = n_labels,
-        self.n_batch = n_batch,
-        self.n_latent = n_latent,
-        self.n_hidden = n_hidden,
-        self.n_cont_cov = n_cont_cov,
+        self.n_input = n_input
+        self.n_labels = n_labels
+        self.n_batch = n_batch
+        self.n_latent = n_latent
+        self.n_hidden = n_hidden
+        self.n_cont_cov = n_cont_cov
         self.dropout_rate = dropout_rate
 
-    def xavier_init(self, shape):
+    def xavier_init(
+        self, 
+        shape
+    ):
         in_dim = shape[0]
         xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
         return tf.random.normal(shape=shape, stddev=xavier_stddev)
 
-    def getQzX(
+    def encoder(
         self,
     ):
-        X = tf.placeholder(tf.float32, shape=[None, self.n_input])
+        X = tf.keras.Input(shape=(None,self.n_input), dtype=tf.dtypes.float32)
 
-        Q_W1 = tf.Variable(self.xavier_init([self.n_input, self.n_hidden]))
-        Q_b1 = tf.Variable(tf.zeros(shape=[self.n_hidden]))
+        Q_W1 = tf.constant(self.xavier_init([self.n_input, self.n_hidden]))
+        Q_b1 = tf.constant(tf.zeros(shape=[self.n_hidden]))
 
-        Q_W2_mu = tf.Variable(self.xavier_init([self.n_hidden, self.n_latent]))
-        Q_b2_mu = tf.Variable(tf.zeros(shape=[self.n_latent]))
+        Q_W2_mu = tf.constant(self.xavier_init([self.n_hidden, self.n_latent]))
+        Q_b2_mu = tf.constant(tf.zeros(shape=[self.n_latent]))
 
-        Q_W2_sigma = tf.Variable(self.xavier_init([self.n_hidden, self.n_latent]))
-        Q_b2_sigma = tf.Variable(tf.zeros(shape=[self.n_latent]))
+        Q_W2_sigma = tf.constant(self.xavier_init([self.n_hidden, self.n_latent]))
+        Q_b2_sigma = tf.constant(tf.zeros(shape=[self.n_latent]))
 
         h = tf.nn.relu(tf.matmul(X, Q_W1) + Q_b1)
         z_mu = tf.matmul(h, Q_W2_mu) + Q_b2_mu
@@ -52,23 +51,47 @@ class VAE():
 
         return z_mu, z_logvar
 
-    def getPXz(
-        self
+    def decoder(
+        self,
+        z
     ):
-        z = tf.placeholder(tf.float32, shape=[None, self.n_latent])
+        P_W1 = tf.constant(self.xavier_init([self.n_latent, self.n_hidden]))
+        P_b1 = tf.constant(tf.zeros(shape=[self.n_hidden]))
 
-        P_W1 = tf.Variable(self.xavier_init([self.n_latent, self.n_hidden]))
-        P_b1 = tf.Variable(tf.zeros(shape=[self.n_hidden]))
-
-        P_W2 = tf.Variable(self.xavier_init([self.n_hidden, self.n_input]))
-        P_b2 = tf.Variable(tf.zeros(shape=[self.n_input]))
+        P_W2 = tf.constant(self.xavier_init([self.n_hidden, self.n_input]))
+        P_b2 = tf.constant(tf.zeros(shape=[self.n_input]))
 
         h = tf.nn.relu(tf.matmul(z, P_W1) + P_b1)
         logits = tf.matmul(h, P_W2) + P_b2
         prob = tf.nn.sigmoid(logits)
+
         return prob, logits
+
+    def sample_z(self, mu, log_var):
+        eps = tf.random_normal(shape=tf.shape(mu))
+        return mu + tf.exp(log_var / 2) * eps
 
     def train(
         self
     ):
-        pass
+        z = tf.keras.Input(shape=(None,self.n_latent), dtype=tf.dtypes.float32)
+        X = tf.keras.Input(shape=(None,self.n_input), dtype=tf.dtypes.float32)
+
+        z_mu, z_logvar = self.encoder()
+        z_sample = self.sample_z(z_mu,z_logvar)
+        _, logits = self.decoder(z_sample)
+
+        X_samples, _ = self.decoder(z)
+
+        # E[log P(X|z)]
+        recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=X), 1)
+        # D_KL(Q(z|X) || P(z))
+        kl_loss= 0.5 * tf.reduce_sum(tf.exp(z_logvar) + z_mu**2 - 1. - z_logvar, 1)
+        # VAE loss
+        vae_loss = tf.reduce_mean(recon_loss + kl_loss)
+
+        solver = tf.train.AdamOptimizer().minimize(vae_loss)
+
+        session = tf.Session()
+        session.run(tf.global_variables_initializer())
+
