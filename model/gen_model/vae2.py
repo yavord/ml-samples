e0b18afdb06d319a9d7c34a31dfc7fdbab999809
yav-d <yavor.d0@gmail.com>
2021-11-08 13:06:53 +0200

    vae tf2 loss wip

---
+++ model/gen_model/vae2.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import tensorflow.keras as tfk
+import tensorflow.keras.layers as tfkl
+
+
+tfkl.Dense #encoder ouput
+tfkl.Conv2DTranspose # decoder output
+
+# loss
+tf.GradientTape
+tf.nn.sigmoid_cross_entropy_with_logits
+
+def vae_loss(x, model, analytic_kl = True, kl_weight = 1.0):
+    z_sample, mu, sd = model.encode(x)
+    x_recons_logits = model.decoder(z_sample)
+
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(
+        labels= x,
+        logits= x_recons_logits
+    )
+
+    neg_log_likelihood = tf.math.reduce_sum(
+        cross_ent, 
+        axis=[1,2,3]
+        )
+
+    if analytic_kl:
+        kl_divergence = -0.5 * tf.math.reduce_sum(
+            1 + tf.math.log(tf.math.square(sd)) - tf.math.square(mu) - tf.math.square(sdaxis=1)
+        )
+    else:
+        # TODO: add Monte Carlo approximation
+        pass
+
+    elbo = tf.math.reduce_mean(-kl_weight * kl_divergence - neg_log_likelihood)
+    return -elbo
